14d1cadf122ea5f9a5986d6e1b77ba730f76ab89
Ezequiel Heick <ezequielheick@gmail.com>
2024-07-26 15:02:59 -0300

    certifications responsive

--- src/components/cert/Certifications.jsx
+++ src/components/cert/Certifications.jsx
@@ -7,32 +7,32 @@ function Certifications() {
   return (
     <div className='relative overflow-hidden bg-gradient-to-r from-cyan-950 via-blue-800 to-cyan-950'>
         <Nav/>
-        <div className="relative my-40 ml-44 font-platypi text-gray-300 text-xl animate-fade-right animate-delay-300">
+        <div className="lg:relative lg:my-40 lg:ml-44 lg:text-xl font-platypi text-gray-300 animate-fade-right animate-delay-300 xs:mt-32 xs:min-h-full">
             
-            <div className="flex my-8 mr-24 ">
+            <div className="lg:flex lg:my-8 lg:mr-24">
               <AdvancedImage cldImg={henry} className="diploma"/>
-              <p className="content-center ml-8">Soy Henry: Desarrollo web fullstack en lenguajes y tecnologías como JavaScript, HTML, CSS, React, Redux, Node, Express, PostgreSQL, Sequelize.</p><br/>
+              <p className="lg:relative lg:content-center lg:ml-8 xs:relative xs:ml-24 xs:mr-4 xs:-top-20">Soy Henry: Desarrollo web fullstack en lenguajes y tecnologías como JavaScript, HTML, CSS, React, Redux, Node, Express, PostgreSQL, Sequelize.</p><br/>
             </div>
 
-            <div className="flex my-8 mr-24 ">
+            <div className="lg:flex lg:my-8 lg:mr-24 xs:mt-10">
               <AdvancedImage cldImg={efset} className="diploma"/>
-              <p className="content-center ml-8">EF SET: Nivel de ingles en lectura, escritura y escucha C2 Abril 2024</p><br/>
+              <p className="lg:relative lg:content-center lg:ml-8 lg:top-0 xs:relative xs:ml-24 xs:-top-20">EF SET: Nivel de ingles en lectura, escritura y escucha C2 Abril 2024</p><br/>
             </div>
             
-            <div className="flex my-8 mr-24 ">
+            <div className="lg:flex lg:my-8 lg:mr-24 xs:relative xs:mt-10">
               <AdvancedImage cldImg={cm} className="diploma"/>
-              <p className="content-center ml-8">Community manager Curso de especializacion en manejo de redes sociales Octubre 2022 </p><br/>
+              <p className="lg:relative lg:content-center lg:ml-8 lg:top-0 xs:relative xs:ml-24 xs:-top-16 xs:text-yellow-500">Community manager Curso de especializacion en manejo de redes sociales Octubre 2022 </p><br/>
             </div>
             
-            <div className="flex my-8 mr-24 ">
-              <AdvancedImage cldImg={lider} className="diploma"/>
-              <p className="content-center ml-8">Programa de especializacion Lideres para una nueva Gobernanza: El programa de especialización se centra en ofrecer una experiencia de aprendizaje integral que prepare a las y los líderes a sortear de manera efectiva y eficaz los desafíos de un entorno cambiante y en constante evolución Los módulos temáticos buscan, en primer lugar, profundizar en la capacidad de liderar en tiempos de cambios, explorando el nuevo paradigma del liderazgo y fomentando el autoliderazgo como base fundamental; en segundo lugar, orientar el enfoque hacia el liderazgo relacional, destacando la importancia de la colaboración y la creación conjunta; y finalmente, concluir con el concepto de liderazgo transformacional fundamental para abordar la gestión constructiva de los conflictos hacia la promoción de la paz, a través de la negociación y el diálogo efectivo.
+            <div className="lg:flex lg:my-8 lg:mr-24 xs:relative xs:mt-10">
+              <AdvancedImage cldImg={lider} className="diploma xs:absolute"/>
+              <p className="lg:relative lg:content-center lg:ml-8 lg:top-0 xs:relative xs:ml-24 xs:mr-4 xs:top-3 xs:text-red-600">Programa de especializacion Lideres para una nueva Gobernanza: El programa de especialización se centra en ofrecer una experiencia de aprendizaje integral que prepare a las y los líderes a sortear de manera efectiva y eficaz los desafíos de un entorno cambiante y en constante evolución Los módulos temáticos buscan, en primer lugar, profundizar en la capacidad de liderar en tiempos de cambios, explorando el nuevo paradigma del liderazgo y fomentando el autoliderazgo como base fundamental; en segundo lugar, orientar el enfoque hacia el liderazgo relacional, destacando la importancia de la colaboración y la creación conjunta; y finalmente, concluir con el concepto de liderazgo transformacional fundamental para abordar la gestión constructiva de los conflictos hacia la promoción de la paz, a través de la negociación y el diálogo efectivo.
               El objetivo de esta especialización es que las y los participantes puedan adquirir las habilidades y las herramientas necesarias para desenvolverse como líderes sociales en los nuevos escenarios generados por las recientes transformaciones políticas, sociales y tecnológicas. </p><br/>
             </div>
             
-            <div className="flex my-8 mr-24 ">
-              <AdvancedImage cldImg={redes}  className="diploma"/>
-              <p className="content-center ml-8 ">Redes sociales: ¿Como comunicar gestión?: Este programa tiene como objetivo comprender la teoría y los conceptos de la nueva comunicación política digital. El programa está orientado a desarrollar las herramientas para la adecuada implementación del uso de datos en el diseño y ejecución de políticas públicas.</p><br/>
+            <div className="lg:flex lg:my-8 lg:mr-24 xs:relative xs:mt-10">
+              <AdvancedImage cldImg={redes}  className="diploma xs:absolute"/>
+              <p className="lg:relative lg:content-center lg:ml-8 lg:top-0 xs:relative  xs:ml-24 xs:mr-4 xs:top-3 xs:text-green-500">Redes sociales: ¿Como comunicar gestión?: Este programa tiene como objetivo comprender la teoría y los conceptos de la nueva comunicación política digital. El programa está orientado a desarrollar las herramientas para la adecuada implementación del uso de datos en el diseño y ejecución de políticas públicas.</p><br/>
             </div>
         </div>
         <Footer/>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <div className='text-white bg-transparent'><hr />
         <div>
-          <ul className="lg:mt-8 lg:left-36 lg:text-lg   xs:h-36 xs:text-sm xs:ml-4">
+          <ul className="lg:h-28 lg:mt-8 lg:left-36 lg:text-lg   xs:h-36 xs:text-sm xs:ml-4">
             <li className="underline xs:mt-1"><Link to='/'>Inicio</Link></li>
             <li className="underline xs:mt-1"><Link to='/certifications'>Estudios y certificaciones</Link></li>
             {/* <li className="underline"><Link to='/projects'>Proyectos</Link></li> */}
@@ -15,19 +15,22 @@ function Footer() {
           </ul>
         </div>
         
-        <div className="lg:left-96 lg:ml-28 lg:bottom-24   xs:relative xs:bottom-16 xs:ml-4">
-          <AdvancedImage cldImg={gmail} className='lg:size-14 lg:ml-40 xs:size-10 xs:ml-40 xs:-mt-20'/>
-          <p className="lg:mt-4 xs:hidden">correo:</p>
-          <p className="lg:mt-4 lg:ml-2 xs:-mt-7 xs:ml-48 xs:text-xs">ezequielheick@gmail.com</p>
-          <AdvancedImage cldImg={whatsapp} className='lg:p-2 lg:size-14 xs:size-8 xs:ml-41 xs: xs:mt-2'/>
-          <p className="lg:mt-4 xs:hidden">whatsapp:</p>
-          <p className="lg:mt-4 lg:ml-2 xs:-mt-6 xs:ml-48 xs:text-xs">+54 011 34637208</p>
+        <div className="lg:left-96 lg:ml-28 lg:bottom-2   xs:relative xs:bottom-16 xs:ml-4">
+          <AdvancedImage cldImg={gmail} className='lg:relative lg:size-14 lg:ml-28 lg:-top-4 xs:size-10 xs:ml-40 xs:-mt-20'/>
+          <p className="lg:relative lg:ml-44 lg:text-lg lg:-top-7 xs:-mt-7 xs:ml-48 xs:text-xs">ezequielheick@gmail.com</p>
+          <AdvancedImage cldImg={whatsapp} className='lg:relative lg:p-2 lg:size-14 lg:ml-28 lg:-top-8 xs:size-8 xs:ml-41 xs: xs:mt-2'/>
+          <p className="lg:relative lg:ml-44 lg:text-lg lg:-top-12 xs:-mt-6 xs:ml-48 xs:text-xs">+54 011 34637208</p>
         </div>
      
           
-        <div className="lg:absolute lg:flex lg:w-28 lg:bottom-24 lg:right-20 lg:gap-4 xs:absolute xs:flex xs:bottom-3 xs:left-52">
-          <a href="https://www.linkedin.com/in/ezequielheick/" target="_blank"> <AdvancedImage cldImg={linkedin} className='size-10 rounded-lg ml-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' /></a>
-          <a href="https://github.com/HeickEzequiel" target="_blank"> <AdvancedImage cldImg={github} className='size-10 rounded-lg ml-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' /></a>
+        <div className="lg:relative lg:max-w-7xl lg:grid lg:gap-4 xs:absolute xs:flex xs:bottom-3 xs:left-52">
+          <a href="https://www.linkedin.com/in/ezequielheick/" target="_blank"> <AdvancedImage cldImg={linkedin} 
+          
+          className='lg:absolute lg:right-20 lg:bottom-24 lg:size-10  size-10 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-150 duration-300' /></a>
+          
+          <a href="https://github.com/HeickEzequiel" target="_blank"> <AdvancedImage cldImg={github} 
+          
+          className='lg:absolute lg:right-20 lg:bottom-12 lg:size-10 size-10 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-150 duration-300' /></a>
         </div>
     </div>
   )
