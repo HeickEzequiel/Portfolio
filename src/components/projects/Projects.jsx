86cea67a1fa29258755386b5b5a6393d5178e471
Ezequiel Heick <ezequielheick@gmail.com>
2024-10-17 13:54:18 -0300

    arreglando deploy

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,12 +1,9 @@
-import Footer from "../footer/Footer"
-import Nav from "../nav/Nav"
+
 
 function Projects() {
   return (
     <div className='relative overflow-hidden bg-gradient-to-r from-cyan-950 via-blue-800 to-cyan-950'>
-        <Nav/>
         <div>Proyectos</div>
-        <Footer/>
     </div>
   )
 }
